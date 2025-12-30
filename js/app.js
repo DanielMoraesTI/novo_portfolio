3f0ba352b53d241b47c412417a0406ec65aaf294
Daniel Fernandes de Moraes <ti.danielmoraes@gmail.com>
2025-12-30 12:16:30 +0000

    Criado menu hamburguer para que seja utilizado quando o ecrã for menos que 768

--- js/app.js
+++ js/app.js
@@ -2,3 +2,17 @@ function enviarForm(event) {
     event.preventDefault();
     alert("Obrigado pelo contacto! Responderei a sua dúvida sobre o tema escolhido em breve. Tenha um ótimo dia!");
 };
+
+const menuToggle = document.querySelector('.menu-toggle');
+const lista = document.querySelector('.lista');
+
+menuToggle.addEventListener('click', () => {
+    lista.classList.toggle('active');
+});
+
+    // Opcional: Fecha o menu ao clicar num link
+document.querySelectorAll('.lista a').forEach(link => {
+    link.addEventListener('click', () => {
+        lista.classList.remove('active');
+    });
+});
